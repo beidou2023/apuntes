0bdad8687e266c35b9652f5912de62f9fbe20a43
beidou2023 <131274568+beidou2023@users.noreply.github.com>
2024-04-18 00:14:16 -0400

    Create Vehiculo.cs

---
+++ PGR-II/repasoVehiculo/Vehiculo.cs
@@ -0,0 +1,20 @@
+namespace Examen2
+{
+    internal class Vehiculo
+    {
+        public string placa;
+        public string marca;
+        public double precio;
+
+        public Vehiculo(string placa, string marca, double precio)
+        {
+            this.placa = placa;
+            this.marca = marca;
+            this.precio = precio;
+        }
+        public Vehiculo()
+        {
+
+        }
+    }
+}
